5b3dbb96eacc959ef124d78d961f4a60ee278f71
Abubakar <abubakarsiddikl.info@gmail.com>
2024-04-03 11:23:30 +0600

    footer data input

--- src/pages/components/Footer.jsx
+++ src/pages/components/Footer.jsx
@@ -1,10 +1,45 @@
-
 const Footer = () => {
-    return (
+  return (
+    <div>
+      <div>
         <div>
-            <h1>footer</h1>
+          <img src="/images/logo.webp" alt="" />
+          <p>
+            Beautifully designed, expertly crafted Flutter components and templates, to jumpstart
+            your projects and speed up your development process.
+          </p>
+          <div>
+            <img src="/images/x.webp" alt="" />
+            <img src="/images/linkedin.webp" alt="" />
+            <img src="/images/instragram.webp" alt="" />
+            <img src="/images/facebook.webp" alt="" />
+          </div>
         </div>
-    );
+        <div>
+          <div>
+            <h6>Explore</h6>
+            <p>Blogs</p>
+            <p>Templates</p>
+            <p>Screens</p>
+          </div>
+          <div>
+            <h6>Support</h6>
+            <p>Pricing</p>
+            <p>Become a sponsor</p>
+            <p>Hire top Flutter Devs</p>
+          </div>
+        </div>
+      </div>
+      <div>
+        <p>Copyright 2024 TheFlutterLibrary. All rights reserved.</p>
+        <div>
+          <p> Privacy Policy </p>
+          <p> Terms & Conditions </p>
+          <p> Cookie Policy</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
