68a69b7cf73681d39e9528ecc294beab656d361d
Abubakar <abubakarsiddikl.info@gmail.com>
2024-04-03 13:44:06 +0600

    header

--- src/pages/components/Header.jsx
+++ src/pages/components/Header.jsx
@@ -1,10 +1,17 @@
-
 const Header = () => {
-    return (
-        <div>
-            <h1>header</h1>
-        </div>
-    );
+  return (
+    <div className="flex mt-5">
+      <div className="flex items-center	 bg-slate-50 me-auto py-2 px-4 rounded-3xl w-1/4	">
+        <img src="/images/searchIcon.webp" alt="" className="max-w-6 max-h-6 me-2" />
+        <input type="text" name="" id="" placeholder="search..." className="bg-slate-50" />
+      </div>
+      <div className="flex items-center	">
+        <img src="/images/brightnessIcon.webp" alt="" className="max-w-6 max-h-6 me-4" />
+        <button className="px-4 py-2 bg-slate-50 rounded-3xl me-4">Login</button>
+        <button className="px-4 py-2 bg-zinc-950 text-indigo-50 rounded-3xl">Get Pro</button>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
